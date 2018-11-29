9c3ab28d1d897f9a17226a66bd33cf048f0c9d58
Calvin Smith <cjsmith@cs.wisc.edu>
2018-11-29 17:00:08 -0600

    norm_scaling graph generation cleaned up

--- experiments/norm_scaling/generate_graph.py
+++ experiments/norm_scaling/generate_graph.py
@@ -22,7 +22,7 @@ args = parser.parse_args()
 # load up the data
 print("Loading data...")
 with open(args.data, "r") as f:
-    data = pd.DataFrame.from_csv(f)
+    data = pd.read_csv(f)
 print("...done.")
 
 # we need to scale data tho
@@ -38,89 +38,45 @@ def split_data(data):
     not_data = data[data["norm"] == False]
     return norm_data, not_data
 
-def get_stats(data):
-    timing_data = data.groupby("size")["time"]
-
-    means = timing_data.mean()
-    stdev = timing_data.std()
-
-    return means, stdev
-
-# get a run
-def sample_from_stats(means, stdev):
-    output = []
-    for m, std in zip(means, stdev):
-        # try:
-        #     value = np.random.normal(m, std)
-        # except:
-        #     value = m
-        value = m
-        output.append(max(value, 0))
-    return output
-
-# construct a time-series
-def make_frame(means, stdev, runs=100):
-    output = []
-    for i in range(runs):
-        output.append(sample_from_stats(means, stdev))
-    return output
-
-# defining how we extract the relevant data and make tha graphs
-# deprecated
-def generate_graph(data, experiment):
-    # unpack the relevant experimental stuff
-    strat, kbo = experiment
-    # what are we gonna write this as
-    file_name = "{}_{}.png".format(strat, "kbo" if kbo else "no-kbo")
-    # now pull out the important stuff
-    data = data[data["strat"] == strat]
-    data = data[data["kbo"] == kbo]
-    # split it up into normal-form and not
-    norm_data = data[data["norm"] == True].groupby("size")["time"].mean()
-    not_data = data[data["norm"] == False].groupby("size")["time"].mean()
-    # now we can graph
-    plt.figure()
-    # set some style stuff
-    rc={'axes.labelsize': 28, 'legend.fontsize': 24, 'axes.titlesize': 36, 'xtick.labelsize': 20, 'ytick.labelsize': 20}
-    sns.set(style="white", rc=rc)
-    # plot the data
-    plt.plot(norm_data, label="Normal")
-    plt.plot(not_data, label="Removed")
-    plt.legend()
-    plt.xlabel("Program size")
-    plt.ylabel("Time (in seconds)")
-    # and save it to file
-    plt.savefig(os.path.join(args.output, file_name))
-
 for exp in EXPERIMENTS:
     # filter the data and split
     exp_data = filter_data(*exp, data)
-    norm, pruned = split_data(exp_data)
+
+    norm = exp_data[exp_data["norm"] == True]
+    pruned = exp_data[exp_data["norm"] == False]
 
     # make a simple graph
 
     # style stuff
-    rc={'axes.labelsize': 28, 'legend.fontsize': 24, 'axes.titlesize': 36, 'xtick.labelsize': 20, 'ytick.labelsize': 20}
-    sns.set(style="white", rc=rc)
+    # rc={'axes.labelsize': 28, 'legend.fontsize': 24, 'axes.titlesize': 36, 'xtick.labelsize': 20, 'ytick.labelsize': 20}
+    sns.set(style="white")
+    sns.set_style("ticks")
+    sns.set_context("paper", font_scale=2.7)
 
     # clear the plots
-    sns.plt.figure()
+    plt.figure()
 
     # extract and create confidence plots
-    norm_ax = sns.tsplot(make_frame(*get_stats(norm), 100), color="r")
-    pruned_ax = sns.tsplot(make_frame(*get_stats(pruned), 100), ax=norm_ax)
+    # norm_ax = sns.lineplot(make_frame(*get_stats(norm), 100), color="r")
+    # pruned_ax = sns.lineplot(make_frame(*get_stats(pruned), 100), ax=norm_ax)
+    norm_ax = sns.lineplot(data=norm, x="size", y="time", color="r", ci=None, label="normal")
+    pruned_ax = sns.lineplot(data=pruned, x="size", y="time", color="b", ax=norm_ax, ci=None, label="removed")
 
     # format the axes
     norm_ax.set_xlabel("Program size")
-    norm_ax.set_ylabel("Time (microseconds)")
+    norm_ax.set_ylabel("Time (in μs)")
+
+    import matplotlib.lines as mlines
+    norm_handle = mlines.Line2D([], [], color="r", label="normal")
+    pruned_handle = mlines.Line2D([], [], color="b", label="removed")
 
     # set up a legend
-    sns.plt.legend([norm_ax, pruned_ax], labels=["normal", "removed"])
+    plt.legend(handles=[norm_handle, pruned_handle])
 
     # minor formatting
-    sns.plt.tight_layout()
+    plt.tight_layout()
     sns.despine()
 
     # and save
-    file_name = "{}_{}.png".format(exp[0], "kbo" if exp[1] else "no-kbo")
-    sns.plt.savefig(os.path.join(args.output, file_name))
+    file_name = "{}_{}.pdf".format(exp[0], "kbo" if exp[1] else "no-kbo")
+    plt.savefig(os.path.join(args.output, file_name))
